f7ce88bf579aecc9d0a0a9adabd811942ea0a234
初始化 <itheima@itcast.cn>
2022-09-06 16:13:20 +0800

    feat:新增特性

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>111</div>
+  <div>
+    <span class="toutiao toutiao-gengduo"></span>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="box">登录</div>
+  </div>
 </template>
 
 <script>
+//1. 下载vant
+//1.1 - yarn add vant@latest-v2 -S
+
+// 2. 让rem动起来 下载适配窗口
+// 2.1 yarn add amfe-flexible
+
+// 3.下载yarn add postem
 export default {};
 </script>
 
-<style>
+<style scoped>
+.box {
+  width: 2rem;
+  height: 2rem;
+  background-color: pink;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 2.2引入flexible
+import 'amfe-flexible'
+// 引入css
+import '@/assets/fonts/iconfont.css'
+
+// 1.2引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
